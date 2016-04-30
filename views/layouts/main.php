3641bcdc72e4582e6c37f5ab96dfa9b12043a5a2
Vadim Cpp <vadim.cpp@gmail.com>
2016-04-30 13:26:41 +0200

    Add empty layout for main page

---
+++ views/layouts/main.php
@@ -0,0 +1,30 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+<?php
+//
+// TODO: what's up here? Nothing. This is empty layout.
+//
+?>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
