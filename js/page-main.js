c1fec59cfed7f9ed68140ea0d19ed28d65627bf4
Nikolai Mavrenkov <koluch@koluch.ru>
2016-05-03 18:17:29 +0300

    New global styles; remove hello-world react code

--- js/page-main.js
+++ js/page-main.js
@@ -3,10 +3,3 @@ import {createElement} from "react"
 
 Array.prototype.slice.apply(document.querySelectorAll(".js-disabled"))
     .forEach((node) => node.parentNode.removeChild(node))
-
-document.addEventListener("DOMContentLoaded", () => {
-    ReactDom.render(
-        createElement("h1", null, "Hello world"),
-        document.getElementById("react-container")
-    )
-})
