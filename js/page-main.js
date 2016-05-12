951bd35be0bd73e06b54754cb2d0f3447ae09f44
Nikolai Mavrenkov <koluch@koluch.ru>
2016-05-12 21:58:01 +0300

    Native implementation

--- js/page-main.js
+++ js/page-main.js
@@ -1,5 +1,24 @@
-import ReactDom from "react-dom"
-import {createElement} from "react"
-
 Array.prototype.slice.apply(document.querySelectorAll(".js-disabled"))
     .forEach((node) => node.parentNode.removeChild(node))
+
+let value = 1000
+
+Array.prototype.slice.apply(document.querySelectorAll(".common-donation_amount")).forEach((input) => {
+
+    const button = Array.prototype.slice.apply(document.querySelectorAll(".common-donation_donate-button"))[0];
+
+    input.addEventListener("focus", (_e) => {
+        input.value = value
+    })
+    input.addEventListener("input", (e) => {
+        const newValue = Number(e.target.value)
+        if (!Number.isNaN(newValue)) { //todo: need polyfill?
+            value = newValue
+        }
+        input.value = value | 0
+        button.disabled = value <= 0
+    })
+    input.addEventListener("blur", (_e) => {
+        input.value = value + " ₽"
+    })
+})
