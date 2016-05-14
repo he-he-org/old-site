2f4a8de5e9ab05574b667899097840ad2c2351fa
Nikolai Mavrenkov <koluch@koluch.ru>
2016-05-14 22:00:50 +0300

    Make donate form works, make fee calculation, show some helpful message

--- js/page-main.js
+++ js/page-main.js
@@ -5,7 +5,11 @@ import {merge} from "functional-utils"
 const initialState = {
     value: 1000,
     focused: false,
+    paymentType: "PC",
 }
+
+const PC_COEFFICIENT = 0.005
+const AC_COEFFICIENT = 0.002
 
 const donationFormReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -31,14 +35,26 @@ const donationFormReducer = (state = initialState, action) => {
 document.addEventListener("DOMContentLoaded", () => {
 
 
-    Array.from(document.querySelectorAll(".common-donation")).forEach((form) => {
-        const input = form.querySelectorAll(".common-donation_amount")[0]
-        const button = form.querySelectorAll(".common-donation_donate-button")[0]
+    Array.from(document.querySelectorAll(".common-donation")).forEach((formDiv) => {
+        const form = formDiv.querySelector(".common-donation_form")
+
+        const tips = formDiv.querySelector(".common-donation_tips")
+        const input = formDiv.querySelector(".common-donation_amount")
+        const button = formDiv.querySelector(".common-donation_donate-button")
+
+        const aInput = form.querySelector("input[name=a]")
+        const sumInput = form.querySelector("input[name=sum]")
+        const amountDueInput = form.querySelector("input[name=amount_due]")
 
         // Configure rendering
         const store = createStore(donationFormReducer)
         const render = () => {
-            const {value, focused} = store.getState()
+            const {value, focused, paymentType} = store.getState()
+
+            if (paymentType === "MC") {
+                throw new Error("MC payment type is not supported yet")
+            }
+
             if (focused) {
                 input.value = value
             }
@@ -46,6 +62,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 input.value = value + " ₽"
             }
             button.disabled = !(value > 0)
+
+            const a = paymentType === "PC" ? PC_COEFFICIENT : AC_COEFFICIENT
+            const amountDue = Math.round((value - value * (a / (1 + a))) * 100) / 100
+            const fee = Math.round((value - amountDue) * 100) / 100
+
+            tips.innerText = `Будет переведено ${amountDue} ₽ (комиссия ${fee} ₽)`
+            sumInput.value = value
         }
         store.subscribe(render)
         render()
