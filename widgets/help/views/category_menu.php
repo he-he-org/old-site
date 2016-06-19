4df98f67418333849dcac2f8fe703d80f860226a
Nikolai Mavrenkov <koluch@koluch.ru>
2016-06-19 18:40:55 +0300

    Fix navigation bug

--- widgets/help/views/category_menu.php
+++ widgets/help/views/category_menu.php
@@ -7,7 +7,7 @@ use app\helpers\RouteHelper;
 <div class="category-menu">
     <?php $mainSection = Yii::$app->request->getSectionPart(0, 'package'); ?>
     <?php foreach ($items as $item) { ?>
-        <?php $isActive = Yii::$app->request->getSectionPart(1, 'press') === $item['section']; ?>
+        <?php $isActive = Yii::$app->request->getSectionPart(1, 'pharmacy') === $item['section']; ?>
         <a href="<?= Url::toRoute(["help/$mainSection/${item['section']}"]) ?>" class="category-menu_item<?= $isActive ? ' category-menu_item--active' : '' ?>">
             <?= $item['title'] ?>
         </a>
