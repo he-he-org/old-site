685c9370736b80a79a241b805c7792c7d006684c
Nikolai Mavrenkov <koluch@koluch.ru>
2016-07-23 17:32:17 +0300

    Rename folder

--- js/entry-admin.js
+++ js/entry-admin.js
@@ -3,7 +3,7 @@ require("is-nan").shim()
 import ReactDOM from "react-dom"
 import {h} from "react-markup"
 
-import Root from "./_admin/root"
+import Root from "./admin/root"
 
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(h(Root), document.getElementById("react"))
