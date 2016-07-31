04660752696b8e3b0dc2b22046862199f23f4b26
Nikolai Mavrenkov <koluch@koluch.ru>
2016-07-31 15:57:54 +0300

    Fix asset

--- assets/AdminAsset.php
+++ assets/AdminAsset.php
@@ -13,10 +13,8 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class AdminAsset extends AssetBundle
+class AdminAsset extends BaseAsset
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
     public $css = [
         'css/admin.css',
     ];
