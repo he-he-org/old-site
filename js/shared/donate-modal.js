fa31c2747ff546eb9c30a4873b94faaafe3437a5
Nikolai Mavrenkov <koluch@koluch.ru>
2016-09-01 00:44:17 +0300

    Close window by escape

--- js/shared/donate-modal.js
+++ js/shared/donate-modal.js
@@ -5,6 +5,18 @@ import prefixer from 'bem-prefixer'
 const bem = prefixer('donate-modal')
 
 export default class DonateModal extends Component {
+
+    componentDidMount = () => {
+        window.addEventListener('keydown', (e) => {
+            if (this.props.displayed) {
+                const ESCAPE_CODE = 27
+                if (e.keyCode === ESCAPE_CODE) {
+                    this.props.onClose()
+                }
+            }
+        })
+    }
+
     render() {
         const {displayed} = this.props
         const {onClose} = this.props
