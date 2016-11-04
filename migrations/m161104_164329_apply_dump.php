756f022401eceef0b0d297b7d29e25525ff60f2d
Nikolai Mavrenkov <koluch@koluch.ru>
2016-11-04 19:44:52 +0300

    Add dump migration

---
+++ migrations/m161104_164329_apply_dump.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m161104_164329_apply_dump extends Migration
+{
+    public function up()
+    {
+        $sql = file_get_contents(__DIR__ . "/dumps/m161104_164329_apply_dump.sql");
+        $this->execute($sql);
+    }
+
+    public function down()
+    {
+        $sql = file_get_contents(__DIR__ . "/dumps/m161023_162024_apply_dump.sql");
+        $this->execute($sql);
+    }
+}
